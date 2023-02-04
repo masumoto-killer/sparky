3bc13395874896d71c1102d8845c5b67389525f0
masumoto-killer <smileykiller97@gmail.com>
2023-02-04 23:24:21 +0900

    disable register

--- storage/framework/views/f1bea6903a6e3723e86f25bec1053baadf747197.php
+++ storage/framework/views/f1bea6903a6e3723e86f25bec1053baadf747197.php
@@ -29,9 +29,9 @@
                     <?php else: ?>
                         <a href="<?php echo e(route('login')); ?>" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                        <?php if(Route::has('register')): ?>
+                        <!-- <?php if(Route::has('register')): ?>
                             <a href="<?php echo e(route('register')); ?>" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        <?php endif; ?>
+                        <?php endif; ?> -->
                     <?php endif; ?>
                 </div>
             <?php endif; ?>
